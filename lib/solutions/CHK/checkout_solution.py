c00e4f3003b231f5f9d7631764dfd0955474a395
sbx_user1051 <sbx_user1051@169.254.66.21>
2024-09-17 07:04:08 +0000

    Tue Sep 17 07:04:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    # presumably deliminated by commas
+    skus = skus.split(",")
+
     # I guess this would be a DB in the real world
     prices = {"A": 50, "B": 30, "C": 20, "D": 15}
     special_offers = {"A": (3, 130), "B": (2, 45)}
 
-    raise NotImplementedError()
