f7630c7ed13490d0728b51306b4cd9845e511b6a
sbx_user1051 <sbx_user1051@169.254.66.21>
2024-09-17 07:07:08 +0000

    Tue Sep 17 07:07:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,10 +1,23 @@
+
+from collections import Counter
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     # presumably deliminated by commas
-    skus = skus.split(",")
+    skus = Counter(skus.split(","))
 
     # I guess this would be a DB in the real world
     prices = {"A": 50, "B": 30, "C": 20, "D": 15}
     special_offers = {"A": (3, 130), "B": (2, 45)}
+
+    total = 0
+
+    for sku, number in skus.items():
+        if offer := special_offers.get(sku):
+            required_for_offer, special_price = offer
+            if number >= required_for_offer:
+
+
+
 
