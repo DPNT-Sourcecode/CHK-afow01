0785645af47f2d19100aad5a0b9f5152a711b83f
sbx_user1051 <sbx_user1051@169.254.66.21>
2024-09-17 07:16:08 +0000

    Tue Sep 17 07:16:08 UTC 2024

--- lib/solutions/CHK/test_checkout.py
+++ lib/solutions/CHK/test_checkout.py
@@ -1,1 +1,14 @@
 from .checkout_solution import checkout
+
+
+def test_checkout_basic():
+    num = checkout("A,A,B,C,D")
+    expected = 50 + 50 + 30 + 20 + 15
+    assert num == expected
+
+
+def test_checkout_one_offer():
+    num = checkout("A,A,B,A,D")
+    expected = 130 + 30 + 15
+    assert num == expected
+
