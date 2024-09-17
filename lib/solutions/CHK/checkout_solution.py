7a7d40b437ac44fc9d76d9085cc90b691e464a71
sbx_user1051 <sbx_user1051@169.254.66.21>
2024-09-17 07:36:21 +0000

    Tue Sep 17 07:36:21 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,7 +9,7 @@ def checkout(skus):
 
     # I guess this would be a DB in the real world
     prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
-    special_offers = {"A": [(5, 200), (3, 130)], "B": [(2, 45)], "E": [(2, 40)]}
+    special_offers = {"A": [(5, 200), (3, 130)], "B": [(2, 45)], "E": [(2, (1, "B"))]}
 
     total = 0
 
@@ -32,5 +32,6 @@ def checkout(skus):
             total += number * rrp
 
     return total
+
 
 
