600d7b99f1e33e15cb8ec4b47523071233ae9327
sbx_user1051 <sbx_user1051@169.254.66.21>
2024-09-17 07:01:08 +0000

    Tue Sep 17 07:01:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,8 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    # I guess this would be a DB in the real world
+    prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+    special_offers = {"A": (3, 130), "B": (2, 45)}
+
     raise NotImplementedError()
--- lib/solutions/HLO/test_hello.py
+++ lib/solutions/HLO/test_hello.py
@@ -3,5 +3,3 @@ from .hello_solution import hello
 
 def test_hello():
     assert hello("John") == "Hello, John!"
-
-
